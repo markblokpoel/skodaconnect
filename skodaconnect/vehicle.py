656cf89db4de801a284397584ccd2ce7cade7659
Mark Blokpoel <35800854+markblokpoel@users.noreply.github.com>
2022-06-06 10:58:36 +0200

    Depature time week schedule bug fix
    Fixed bug where only 'y' from the submitted schedule was passed on to Skoda API ('n's were dropped).

--- skodaconnect/vehicle.py
+++ skodaconnect/vehicle.py
@@ -624,8 +624,8 @@ class Vehicle:
                             data['timersSettings']['timers'][index]['recurringOn'] = []
                             days = schedule.get('days', 'nnnnnnn')
                             for num in range(0, 7):
-                                if days[num] == 'y':
-                                    data['timersSettings']['timers'][index]['recurringOn'].append(days[num])
+#                                if days[num] == 'y':
+                                 data['timersSettings']['timers'][index]['recurringOn'].append(days[num])
                         else:
                             data['timersSettings']['timers'][index]['type'] = 'ONE_OFF'
                             data['timersSettings']['timers'][index]['date'] = schedule.get('date')
